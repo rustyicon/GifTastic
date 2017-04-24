b95c95b32054ba305e62418a5de5457e5bcee800
Sharon Zavala <sharon@MacBook-Pro.local>
2017-04-24 15:11:01 -0700

    sixth

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,104 +1,111 @@
 $(document).ready(function() {
 
 
-	var topics = [ "8 Bit Friends", "8Bit Game", "Trippy 8 Bit", "8 Bit Pizza", "Jurassic Park 8 Bit", "8Bit Unicorn"];
+    var topics = ["8 Bit Friends", "8Bit Game", "Trippy 8 Bit", "8 Bit Pizza", "Jurassic Park 8 Bit", "8Bit Unicorn", "8 Bit Band"];
 
-	
-	for (var i = 0; i < topics.length; i++) {
-				
-		var BitBtn = $("<button class='buttons'>" + topics[i] + "</div>");
 
-		BitBtn.attr("data-theme", topics[i]);
-		
-		$("#new-bit-gif").on("click", "thisATTR", function (event){
-		event.preventDefault();
-		newBtn = $("#userbitInput").val().trim();
-		topics.push(newBtn); 
-		console.log(event)
-		
-		});
-		//console.log(topics[i])
+    for (var i = 0; i < topics.length; i++) {
 
-		$("#bit-btns").append(BitBtn);
-		//var cosplayImage = $("<img>");
-		//cosplayImage.attr("src", topics[i]);
-	};
+        var BitBtn = $("<button class='buttons'>" + topics[i] + "</div>");
 
-	$("button").on("click", function(event){
+        BitBtn.attr("data-theme", topics[i]);
 
-		//console.log(event);
-		var theme = $(this).attr("data-theme");
-		//console.log(this)
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + theme +"&api_key=dc6zaTOxFJmzC&limit=10";
-		
-		$.ajax({
-			url: queryURL,
-			method:"GET"
-		}).done(function(response){
-			//console.log(response)
+        //console.log(topics[i])
 
-			var results = response.data;
+        $("#bit-btns").append(BitBtn);
+        //var cosplayImage = $("<img>");
+        //cosplayImage.attr("src", topics[i]);
+    };
 
-				for (var i = 0; i < results.length; i++) {
+    $(document).on("click", "#new-bit-gif", function(event) {
+        event.preventDefault();
+        newBtn = $("#userbitInput").val().trim();
+        topics.push(newBtn);
+        console.log(event)
 
-					//if (results[c].rating !== "r" && results[c].rating !== "pg-13") {
-					
-					var gifDiv = $("<div class='img'>");
- 
-					var rating = results[i].rating;
+        for (var i = 0; i < topics.length; i++) {
 
-					var p = $("<p>").text("Rating:" + rating);
+            var BitBtn = $("<button class='buttons'>" + topics[i] + "</div>");
 
-					var gifImage = $("<img class='gif'>");
+            BitBtn.attr("data-theme", topics[i]);
 
-					var gifURL = results[i].images.fixed_height_still.url;
+            //console.log(topics[i])
+            
+            $("#bit-btns").append(BitBtn);
+        }
+    });
 
-					var animate = results[i].images.fixed_height.url;
 
-					gifImage.attr("src", gifURL);
+    $(document).on("click", "button", function(event) {
 
-					gifImage.attr("data-state", "still");
+        //console.log(event);
+        var theme = $(this).attr("data-theme");
+        //console.log(this)
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + theme + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-					gifImage.attr("data-animate", animate);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function(response) {
+            //console.log(response)
 
-					//console.log(results[c])
-					gifDiv.append(gifImage);
-					gifDiv.append(p);
+            var results = response.data;
 
-					$("#bits").prepend(gifDiv);
-					//}	
-				}
+            for (var i = 0; i < results.length; i++) {
 
-						//console.log("data-state")
-		$(".gif").on("click", function (event){
-		console.log(event)
-		event.preventDefault();
+                //if (results[c].rating !== "r" && results[c].rating !== "pg-13") {
 
-		var state = $(this).attr("src");
+                var gifDiv = $("<div class='img'>");
 
-		var animate = $(this).attr("data-animate");
-						//var animate = $(this).attr("data-animate");
-						//var obj = JSON.stringify(still);
-							
+                var rating = results[i].rating;
 
-		$(this).attr("src", animate);
-					
-		$(this).attr("data-animate", state);
-						
-		
-		});
+                var p = $("<p>").text("Rating:" + rating);
 
-	});
-	});
+                var gifImage = $("<img class='gif'>");
 
-	/*function addBit (){
-		var newBtn = $("userbitBtn").val();
-		topics.push(newBtn);
-		$("bit-btns").submit();
-	}*/
+                var gifURL = results[i].images.fixed_height_still.url;
+
+                var animate = results[i].images.fixed_height.url;
+
+                gifImage.attr("src", gifURL);
+
+                gifImage.attr("data-state", "still");
+
+                gifImage.attr("data-animate", animate);
+
+                //console.log(results[c])
+                gifDiv.append(gifImage);
+                gifDiv.append(p);
+
+                $("#bits").prepend(gifDiv);
+                //}	
+            }
+
+            //console.log("data-state")
+            $(".gif").on("click", function(event) {
+                console.log(event)
+                event.preventDefault();
+
+                var state = $(this).attr("src");
+
+                var animate = $(this).attr("data-animate");
+                //var animate = $(this).attr("data-animate");
+                //var obj = JSON.stringify(still);
+
+
+                $(this).attr("src", animate);
+
+                $(this).attr("data-animate", state);
+
+
+            });
+
+        });
+    });
+
+    /*function addBit (){
+    	var newBtn = $("userbitBtn").val();
+    	topics.push(newBtn);
+    	$("bit-btns").submit();
+    }*/
 });
-	
-
-
-
-
